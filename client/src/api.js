04c24891b37f172aa1e3a32ba96badbce963b622
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-10 21:41:42 +0530

    added update & delete entry api

--- client/src/api.js
+++ client/src/api.js
@@ -4,16 +4,20 @@ const API = axios.create({
   baseURL: "http://localhost:8000",
 });
 
-export const createEntry=(data)=> API.post("/learning/", data);
-export const getEntries=()=> API.get("/learning/");
-export const getOverview=()=> API.get("/analytics/overview");
-export const getSkills=()=> API.get("/analytics/skills");
-export const getTopicBreakdown=()=> API.get("/analytics/topic-breakdown");
-export const getStudyTime=(mode)=> API.get(`/analytics/study-time?mode=${mode}`);
-export const getStreak=()=> API.get("/analytics/streak");
-export const getMonthlyGoal=()=> API.get("/analytics/monthly-goal");
-export const getInsights=()=> API.get("/analytics/insights");
-export const getAveragePerformance=()=> API.get("/analytics/average-performance");
-export const getConsistency=()=> API.get("/analytics/consistency");
+export const createEntry = (data) => API.post("/learning/", data);
+export const getEntries = () => API.get("/learning/");
+export const updateEntry = (id, data) => API.put(`/learning/${id}`, data);
+export const deleteEntry = (id) => API.delete(`/learning/${id}`);
+export const getOverview = () => API.get("/analytics/overview");
+export const getSkills = () => API.get("/analytics/skills");
+export const getTopicBreakdown = () => API.get("/analytics/topic-breakdown");
+export const getStudyTime = (mode) =>
+  API.get(`/analytics/study-time?mode=${mode}`);
+export const getStreak = () => API.get("/analytics/streak");
+export const getMonthlyGoal = () => API.get("/analytics/monthly-goal");
+export const getInsights = () => API.get("/analytics/insights");
+export const getAveragePerformance = () =>
+  API.get("/analytics/average-performance");
+export const getConsistency = () => API.get("/analytics/consistency");
 
 export default API;

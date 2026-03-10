41f77b895e707adf7abe65146c297c0dbc02830e
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-10 21:52:58 +0530

    added entrylist with update and delete features

---
+++ client/src/components/EntryList.jsx
@@ -0,0 +1,31 @@
+import { FaTrash, FaEdit } from "react-icons/fa";
+
+const EntryList = (entries, editEntry, removeEntry) => {
+  return (
+    <div className="overflow-y-auto">
+      {entries.map((e) => (
+        <div
+          key={e.id}
+          className="border p-3 mb-2 rounded flex justify-between items-center"
+        >
+          <div>
+            <p className="font-semibold">{e.topic}</p>
+            <p className="text-sm">{e.hours}hrs</p>
+          </div>
+          <div className="flex gap-2">
+            <FaEdit
+              className="cursor-pointer text-blue-500"
+              onClick={() => editEntry(e)}
+            />
+            <FaTrash
+              className="cursor-pointer text-red-500"
+              onClick={() => removeEntry(e)}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EntryList;
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import EntryList from "./EntryList"
+
+const Sidebar = ({}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sidebar

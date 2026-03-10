861973ef6c7b41dabdba7bb40fc1e83973213447
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-10 21:00:33 +0530

    fixed import statement error "axios"

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
-import axios from axios;
+import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost:8000",
+  baseURL: "http://localhost:8000",
 });
 
 export const createEntry=(data)=> API.post("/learning/", data);

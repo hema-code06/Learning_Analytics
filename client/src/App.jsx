e3d33712b4ab3a142a9f0570f82cd8513cb32bef
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-11 18:20:49 +0530

    added global toast notification container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import Dashboard from "./pages/Dashboard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Dashboard />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 }

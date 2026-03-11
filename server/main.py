bb5387972534e2a57a458458fdc4d1298ed804e2
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-11 13:14:32 +0530

    enable cors middleware for frontend requests

--- server/main.py
+++ server/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from database import Base, engine
 from routers import learning, analytics
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(learning.router, prefix="/learning", tags=["Learning"])
 app.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
